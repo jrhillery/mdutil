ebbe36389369a211edbe12fc33cd423b8cf02b07
John Hillery <John@localhost>
2018-06-24 15:12:59 -0400

    Allow for account balances to the sixth place past the decimal

--- md-util/src/main/java/com/leastlogic/moneydance/util/MdUtil.java
+++ md-util/src/main/java/com/leastlogic/moneydance/util/MdUtil.java
@@ -29,7 +29,7 @@ import com.infinitekind.moneydance.model.CurrencyType;
  * Collection of common utility methods handy for Moneydance extensions.
  */
 public class MdUtil {
-	private static final double[] centMult = { 1, 10, 100, 1000, 10000 };
+	private static final double[] centMult = { 1, 10, 100, 1000, 10000, 100000, 1000000 };
 
 	/**
 	 * @param security The Moneydance security
