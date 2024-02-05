13c7e2bbf54a2469208a70042df68fd3b2e81499
John Hillery <John@localhost>
2024-02-05 15:37:43 -0500

    Simplify path separator reference

--- MdProjects/src/main/java/gz/jythonhelper/Main.java
+++ MdProjects/src/main/java/gz/jythonhelper/Main.java
@@ -1,8 +1,8 @@
 package gz.jythonhelper;
 
-public class Main {
+import java.io.File;
 
-   public static final String PATH_SEPARATOR = System.getProperty("path.separator");
+public class Main {
 
    private static void generatePy(String outputDir, String... files) {
       EasyJython ej = new EasyJython(outputDir);
@@ -14,7 +14,7 @@ public class Main {
    public static void main(String[] args) {
       if (args.length > 1) {
          String outputDir = args[0];
-         String[] files = args[1].split(PATH_SEPARATOR);
+         String[] files = args[1].split(File.pathSeparator);
          generatePy(outputDir, files);
       } else {
          System.out.println("Missing argument; need <output directory> and <source directories path>");
