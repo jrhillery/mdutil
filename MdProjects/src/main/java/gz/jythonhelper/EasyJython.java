f367d0de7e4353f367a5676781074fb98a747958
John Hillery <John@localhost>
2024-02-08 14:45:29 -0500

    Remove unused code

--- MdProjects/src/main/java/gz/jythonhelper/EasyJython.java
+++ MdProjects/src/main/java/gz/jythonhelper/EasyJython.java
@@ -58,15 +58,6 @@ public class EasyJython {
         classLoader = new URLClassLoader(urls.toArray(new URL[urls.size()]));
     }
 
-    private static String[] getFilesFromArgs(String[] args) {
-        List<String> list = new ArrayList<>();
-        for (String arg : args) {
-            String[] split = arg.split(";|:");
-            list.addAll(Arrays.asList(split));
-        }
-        return list.toArray(new String[list.size()]);
-    }
-
     private static String stripUserDir(String file) {
         String strippedFile;
 
@@ -327,15 +318,6 @@ class PyClassGenerator extends ClassGenerator {
         writeClassesFromInitPy(classStringMap, initPy, resourceURL);
     }
 
-    public void createPyForClass0(Class clazz) throws IOException {
-        File directory = createDirectory(clazz);
-        String classContent = generateClassAsPyClass(clazz);
-        File classFile = new File(directory, INIT_PY);
-        FileWriter fileWriter = new FileWriter(classFile, true);
-        fileWriter.write(classContent);
-        fileWriter.close();
-    }
-
     private Map<String, StringBuilder> readClassesFromInitPy(File initFile) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(initFile));
         String line;
@@ -440,90 +422,5 @@ class PyClassGenerator extends ClassGenerator {
         }
 
         return prefix + String.format(DEF_TPL, m.getName(), sb.toString());
-    }
-}
-
-class PyModuleGenerator extends ClassGenerator {
-    protected PyModuleGenerator(String outputDir) {
-        super(outputDir);
-    }
-
-    public void createPyForClass(Class clazz) throws IOException {
-        File directory = createDirectory(clazz);
-        String classContent = generateClassAsModule(clazz);
-        if (classContent == null) {
-            return;
-        }
-        File classFile = new File(directory, clazz.getSimpleName() + ".py");
-        FileWriter fileWriter = new FileWriter(classFile);
-        fileWriter.write(classContent);
-        fileWriter.close();
-    }
-
-    private String generateMethodForModule(Method m) {
-        Class<?>[] parameterTypes = m.getParameterTypes();
-        StringBuilder sb = new StringBuilder();
-        int modifiers = m.getModifiers();
-        String prefix = "";
-        for (int i = 0; i < parameterTypes.length; i++) {
-            Class<?> type = parameterTypes[i];
-            String typeName;
-            if (type.isArray()) {
-                typeName = type.getComponentType().getSimpleName();
-            } else {
-                typeName = type.getSimpleName();
-            }
-            sb.append(typeName + (i > 0 ? i : "") + "=None");
-            if (i < parameterTypes.length - 1) {
-                sb.append(", ");
-            }
-        }
-
-        return prefix + String.format(DEF_TPL, m.getName(), sb.toString());
-    }
-
-
-    private String generateClassAsModule(Class clazz) {
-        StringBuilder sb = new StringBuilder();
-        Field[] fields = new Field[0];
-
-        try {
-            fields = clazz.getFields();
-        } catch (Throwable e) {
-            System.err.println("Get Field error:" + e.getMessage());
-        }
-        for (Field f : fields) {
-            int modifiers = f.getModifiers();
-            if (Modifier.isPublic(modifiers)) {
-                if (!IGNORED_FIELDS.contains(f.getName())) {
-                    sb.append(indent(makeAField(f), 0));
-                    sb.append(LINE_SEPARATOR);
-                }
-            }
-        }
-        sb.append(LINE_SEPARATOR);
-        sb.append(LINE_SEPARATOR);
-        Method[] methods = new Method[0];
-        try {
-            methods = clazz.getMethods();
-        } catch (Throwable e) {
-            System.err.println("Get Method error:" + e.getMessage());
-        }
-        methods = filterOverrideMethods(methods);
-        for (Method m : methods) {
-            int modifiers = m.getModifiers();
-            if (Modifier.isPublic(modifiers)) {
-                String name = m.getName();
-                if (!IGNORED_METHODS.contains(name)) {
-                    sb.append(indents(generateMethodForModule(m), 0));
-                    sb.append(LINE_SEPARATOR);
-                    sb.append(indents(PASS, 2));
-                    sb.append(LINE_SEPARATOR);
-                    sb.append(LINE_SEPARATOR);
-                    sb.append(LINE_SEPARATOR);
-                }
-            }
-        }
-        return sb.toString();
     }
 }
