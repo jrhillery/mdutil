e3e510b521a129ab5140d4669b512b8e46e9b68d
John Hillery <John@localhost>
2024-09-11 08:49:15 -0400

    Use TreeMap.floorKey to simplify date location logic

--- md-util/src/main/java/com/leastlogic/moneydance/util/SnapshotList.java
+++ md-util/src/main/java/com/leastlogic/moneydance/util/SnapshotList.java
@@ -3,12 +3,11 @@
  */
 package com.leastlogic.moneydance.util;
 
-import java.time.LocalDate;
-import java.util.Comparator;
-import java.util.List;
-
 import com.infinitekind.moneydance.model.CurrencySnapshot;
 import com.infinitekind.moneydance.model.CurrencyType;
+
+import java.time.LocalDate;
+import java.util.TreeMap;
 
 /**
  * Utility class to house a list of snapshots for a Moneydance security.
@@ -16,7 +15,7 @@ import com.infinitekind.moneydance.model.CurrencyType;
 public class SnapshotList {
 
 	private final CurrencyType security;
-	private final List<CurrencySnapshot> snapshots;
+	private final TreeMap<Integer, CurrencySnapshot> snapshots = new TreeMap<>();
 
 	/**
 	 * Sole constructor.
@@ -25,40 +24,20 @@ public class SnapshotList {
 	 */
 	public SnapshotList(CurrencyType security) {
 		this.security = security;
-		this.snapshots = getSnapshots(security);
+
+      security.getSnapshots().forEach(snapShot ->
+			this.snapshots.put(snapShot.getDateInt(), snapShot));
 
 	} // end (CurrencyType) constructor
-
-	/**
-	 * @param security The Moneydance security
-	 * @return Sorted list of snapshots for the specified security
-	 */
-	private static List<CurrencySnapshot> getSnapshots(CurrencyType security) {
-		List<CurrencySnapshot> snapShots = security.getSnapshots();
-		snapShots.sort(Comparator.comparing(CurrencySnapshot::getDateInt));
-
-		return snapShots;
-	} // end getSnapshots(CurrencyType)
 
 	/**
 	 * @param dateInt The desired date
 	 * @return The currency snapshot for our security on the specified date
 	 */
 	public CurrencySnapshot getSnapshotForDate(int dateInt) {
-		int index = this.snapshots.size();
+		Integer snapshotDate = this.snapshots.floorKey(dateInt);
 
-		if (index == 0)
-			return null;
-
-		// start with the latest snapshot
-		CurrencySnapshot candidate = this.snapshots.get(--index);
-
-		while (candidate.getDateInt() > dateInt && index > 0) {
-			// examine the prior snapshot
-			candidate = this.snapshots.get(--index);
-		}
-
-		return candidate;
+		return snapshotDate == null ? null : this.snapshots.get(snapshotDate);
 	} // end getSnapshotForDate(int)
 
 	/**
