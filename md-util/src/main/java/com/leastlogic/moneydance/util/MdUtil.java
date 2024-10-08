cb2cbac793f995df1837d092eb2b3597b604a944
John Hillery <John@localhost>
2024-10-08 17:26:04 -0400

    optimize rate retrieval (only once per snapshot)

--- md-util/src/main/java/com/leastlogic/moneydance/util/MdUtil.java
+++ md-util/src/main/java/com/leastlogic/moneydance/util/MdUtil.java
@@ -18,7 +18,6 @@ import com.infinitekind.moneydance.model.Account;
 import com.infinitekind.moneydance.model.Account.AccountType;
 import com.infinitekind.moneydance.model.AccountBook;
 import com.infinitekind.moneydance.model.AccountUtil;
-import com.infinitekind.moneydance.model.CurrencySnapshot;
 import com.infinitekind.moneydance.model.CurrencyType;
 
 /**
@@ -30,27 +29,27 @@ public class MdUtil {
 	public static final String IBOND_TICKER_PREFIX = "IBond";
 
 	/**
-	 * @param security        The Moneydance security
-	 * @param price           The price for this snapshot:
-	 * @param currentSnapshot Today's currency snapshot for the supplied security
-	 * @param locale          Desired locale
+	 * @param security The Moneydance security
+	 * @param price    The price for the current snapshot
+	 * @param rate     The rate for the current snapshot
+	 * @param locale   Desired locale
 	 * @return Optional String describing correction to the current price
 	 */
 	public static Optional<String> validateCurrentUserRate(CurrencyType security,
-			BigDecimal price, CurrencySnapshot currentSnapshot, Locale locale) {
+			BigDecimal price, double rate, Locale locale) {
 		BigDecimal oldPrice = convRateToPrice(security.getRelativeRate());
 
 		if (price.compareTo(oldPrice) == 0)
 			return Optional.empty();
 
-		security.setRelativeRate(currentSnapshot.getRate());
+		security.setRelativeRate(rate);
 
 		NumberFormat priceFmt = getCurrencyFormat(locale, oldPrice, price);
 
         return Optional.of("Changed %s (%s) current price from %s to %s.".formatted(
 				security.getName(), security.getTickerSymbol(),
 				priceFmt.format(oldPrice), priceFmt.format(price)));
-	} // end validateCurrentUserRate(CurrencyType, BigDecimal, CurrencySnapshot)
+	} // end validateCurrentUserRate(CurrencyType, BigDecimal, double, Locale)
 
 	/**
 	 * @param rate The Moneydance currency rate for a security
